a96dc92649e559cb023c42f860937ae93901a919
Aimerny <bbbxingyu@gmail.com>
2024-07-07 01:10:57 +0800

    feat: :sparkles: 增加分页功能

--- player_last_play/__init__.py
+++ player_last_play/__init__.py
@@ -1,37 +1,40 @@
+import math
 from typing import List
 from mcdreforged.api.all import *
 import datetime
 
-
 __mcdr_server: PluginServerInterface
 data: dict
 
-
 help_msg = '''-------- §a Player Last Play §r--------
 §b!!plp help §f- §c显示帮助消息
-§b!!plp list §f- §c全部玩家列表
+§b!!plp list [index] §f- §c显示玩家列表,index代表页数
 §b!!plp get <player> §f- §c获取玩家的最后游玩时间
 §b!!plp clean <player> §f- §c清除玩家的信息
 -----------------------------------
 '''
 
+
 class Config(Serializable):
     # 一周内没上线属于很活跃
-    active:int = 7
+    active: int = 7
     # 两周内没上线属于较为一般
-    normal:int = 14
-    # 三周内没上线属于基本不活跃
-    inactive:int = 21
-    # 超过三周属于潜水
+    normal: int = 14
+    # 三周内没上线属于基本不活跃,超过三周属于潜水
+    inactive: int = 21
+    # 是否倒序查询,默认从近到远
+    reverse: bool = True
+    # 分页查询的大小,默认10
+    pageSize: int = 10
 
-    reverse:bool = True
 
 config: Config
 
+
 class PlayerInfo:
-    player:str
-    last_date:datetime
-    activity:str
+    player: str
+    last_date: datetime
+    activity: str
 
     def __init__(self, player, last_date):
         self.player = player
@@ -41,7 +44,7 @@ class PlayerInfo:
     def get_activity(self) -> str:
         now = datetime.datetime.now()
         days = (now - self.last_date).days
-        
+
         if days < config.active:
             return 'active'
         elif days >= config.active and days < config.normal:
@@ -49,7 +52,7 @@ class PlayerInfo:
         elif days >= config.normal and days < config.inactive:
             return 'inactive'
         else:
-            # (这么久不上线，危)
+            # (这么久不上线，给红的)
             return 'danger'
 
 
@@ -60,21 +63,23 @@ def on_load(server: PluginServerInterface, old):
     config = server.load_config_simple(target_class=Config)
     data = server.load_config_simple(
         'data.json',
-        default_config = {'player_list': {}},
+        default_config={'player_list': {}},
         echo_in_console=True
     )['player_list']
     server.register_help_message('!!plp', '获取玩家最后一次游玩时间')
 
-    #注册指令
+    # 注册指令
     command_builder = SimpleCommandBuilder()
     command_builder.command('!!plp list', player_list)
+    command_builder.command('!!plp list <index>', player_list)
     command_builder.command('!!plp get <player>', get_player)
     command_builder.command('!!plp clean <player>', clean_player)
     command_builder.command('!!plp help', help_info)
     command_builder.command('!!plp', help_info)
     command_builder.arg('player', Text)
+    command_builder.arg('index', Integer)
     command_builder.register(server)
-    
+
 
 def on_player_left(server: PluginServerInterface, player: str):
     if player.startswith('bot_') or player.startswith('Bot_'):
@@ -94,14 +99,22 @@ def help_info(server):
         server.reply(line)
 
 
-def player_list(server):
-    resp = '------ &a玩家列表 &r------'
+def player_list(server, context):
+    if 'index' in context:
+        index = context['index'] - 1
+    else:
+        index = 0
+    if index < 0:
+        return server.reply("查询页数不能小于1")
+    pagesize = config.pageSize
+    resp = '------ &a玩家列表 &r------\n'
     online_players = get_online_players()
+    result_list = []
     # 先统计在线的玩家
     for player in online_players:
         # 跳过假人
         if not player.startswith('bot_') and not player.startswith('Bot_'):
-            resp = resp + f'\n&r|- &a{player}&r:&a在线'
+            result_list.append(f'&r|- &a{player}&r:&a在线')
 
     # 作排序，按日期从近到远排序
     offline_players = []
@@ -109,18 +122,35 @@ def player_list(server):
         # 只统计不在线的玩家
         if player not in online_players:
             offline_players.append(PlayerInfo(player, datetime.datetime.strptime(data[player], '%Y-%m-%d')))
-    
+
     sorted_off_players = sort_date(offline_players)
     for player in sorted_off_players:
         # 按游玩先后顺序排序
-        resp = resp + f'\n|- &a{player.player}&r:&{get_color_by_activity(player.activity)}{player.last_date.strftime("%Y-%m-%d")}'
+        result_list.append(
+            f'&r|- &a{player.player}&r:&{get_color_by_activity(player.activity)}{player.last_date.strftime("%Y-%m-%d")}')
+    # 查分页
+    total = len(result_list)
+    if total == 0:
+        resp += f'&r>>>>>> 第0页/共0页 <<<<<<'
+    else:
+        pages = math.ceil(total / pagesize)
+        if 0 <= index < pages - 1:
+            cur_page = result_list[index * pagesize: (index + 1) * pagesize]
+        elif index == pages - 1:
+            cur_page = result_list[index * pagesize:]
+        else:
+            # 大于最大的pages
+            return server.reply('超过总页数,无法查询')
+        for msg in cur_page:
+            resp += msg + '\n'
+        resp += f'&r>>>>>> 第{index + 1}页/共{pages}页 <<<<<<'
     server.reply(replace_code(resp))
 
 
 def get_player(server, context):
     player = context['player']
     online_players = get_online_players()
-    resp:str
+    resp: str
     if player in online_players:
         resp = f'玩家&a{player}&r当前&a在线'
     elif player in data:
@@ -135,15 +165,13 @@ def clean_player(server, context):
     if __mcdr_server.get_permission_level(server) < 3:
         resp = f'&c你没有权限清除玩家的最近游玩时间'
     player = context['player']
-    resp:str
+    resp: str
     if player in data:
         del data[player]
         resp = f'已清除玩家&a{player}&r最近的游玩时间'
     else:
         resp = f'当前没有玩家&a{player}&r的游玩时间'
     server.reply(replace_code(resp))
-    
-    
 
 
 # -------------------------
@@ -155,19 +183,21 @@ def save_data(server: PluginServerInterface):
 
 
 def replace_code(msg: str) -> str:
-    return msg.replace('&','§')
+    return msg.replace('&', '§')
 
 
 def get_online_players() -> list:
     online_player_api = __mcdr_server.get_plugin_instance('online_player_api')
     return online_player_api.get_player_list()
 
-def sort_date(player_list:List[PlayerInfo]) -> List[PlayerInfo]:
-    sorted_player_list = sorted(player_list, key= lambda player: player.last_date.timestamp(), reverse=config.reverse)
+
+def sort_date(player_list: List[PlayerInfo]) -> List[PlayerInfo]:
+    sorted_player_list = sorted(player_list, key=lambda player: player.last_date.timestamp(), reverse=config.reverse)
     return sorted_player_list
 
-def get_color_by_activity(activity:str) -> str:
-    if activity == 'active' :
+
+def get_color_by_activity(activity: str) -> str:
+    if activity == 'active':
         # 绿色
         return 'a'
     elif activity == 'normal':
